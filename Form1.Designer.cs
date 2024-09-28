bff7d3bb95e837700019cc4cf640a43e2b96cc34
kiero <kiero@K13R0N>
2024-09-28 14:16:42 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace spudey_executor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.execute = new System.Windows.Forms.Button();
+            this.Inject = new System.Windows.Forms.Button();
+            this.Editor = new FastColoredTextBoxNS.FastColoredTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.robloxopen = new System.Windows.Forms.Label();
+            this.status = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.openfile = new System.Windows.Forms.Button();
+            this.savefile = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Editor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // execute
+            // 
+            this.execute.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.execute.BackColor = System.Drawing.Color.Red;
+            this.execute.Location = new System.Drawing.Point(31, 442);
+            this.execute.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.execute.Name = "execute";
+            this.execute.Size = new System.Drawing.Size(112, 39);
+            this.execute.TabIndex = 0;
+            this.execute.Text = "Execute";
+            this.execute.UseVisualStyleBackColor = false;
+            this.execute.Click += new System.EventHandler(this.execute_Click);
+            // 
+            // Inject
+            // 
+            this.Inject.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.Inject.BackColor = System.Drawing.Color.Red;
+            this.Inject.Location = new System.Drawing.Point(915, 442);
+            this.Inject.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Inject.Name = "Inject";
+            this.Inject.Size = new System.Drawing.Size(136, 39);
+            this.Inject.TabIndex = 1;
+            this.Inject.Text = "Inject";
+            this.Inject.UseVisualStyleBackColor = false;
+            this.Inject.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Editor
+            // 
+            this.Editor.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Editor.AutoCompleteBracketsList = new char[] {
+        '(',
+        ')',
+        '{',
+        '}',
+        '[',
+        ']',
+        '\"',
+        '\"',
+        '\'',
+        '\''};
+            this.Editor.AutoScrollMinSize = new System.Drawing.Size(155, 14);
+            this.Editor.BackBrush = null;
+            this.Editor.BackColor = System.Drawing.Color.Crimson;
+            this.Editor.CharHeight = 14;
+            this.Editor.CharWidth = 8;
+            this.Editor.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.Editor.DisabledColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))), ((int)(((byte)(180)))));
+            this.Editor.ForeColor = System.Drawing.SystemColors.Control;
+            this.Editor.IsReplaceMode = false;
+            this.Editor.Location = new System.Drawing.Point(14, 46);
+            this.Editor.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Editor.Name = "Editor";
+            this.Editor.Paddings = new System.Windows.Forms.Padding(0);
+            this.Editor.SelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.Editor.ServiceColors = ((FastColoredTextBoxNS.ServiceColors)(resources.GetObject("Editor.ServiceColors")));
+            this.Editor.Size = new System.Drawing.Size(923, 373);
+            this.Editor.TabIndex = 2;
+            this.Editor.Text = "--Made by k13r0n";
+            this.Editor.Zoom = 100;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.BackColor = System.Drawing.Color.Red;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(5, 3);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1046, 33);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Spudey executor";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.label1_MouseDown);
+            // 
+            // robloxopen
+            // 
+            this.robloxopen.AutoSize = true;
+            this.robloxopen.BackColor = System.Drawing.Color.Red;
+            this.robloxopen.ForeColor = System.Drawing.SystemColors.Control;
+            this.robloxopen.Location = new System.Drawing.Point(28, 3);
+            this.robloxopen.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.robloxopen.Name = "robloxopen";
+            this.robloxopen.Size = new System.Drawing.Size(84, 13);
+            this.robloxopen.TabIndex = 4;
+            this.robloxopen.Text = "Roblox Open:";
+            this.robloxopen.Click += new System.EventHandler(this.robloxopen_Click);
+            // 
+            // status
+            // 
+            this.status.BackColor = System.Drawing.Color.Red;
+            this.status.ForeColor = System.Drawing.SystemColors.Control;
+            this.status.Location = new System.Drawing.Point(28, 16);
+            this.status.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(192, 20);
+            this.status.TabIndex = 5;
+            this.status.Text = "Status:";
+            this.status.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox1.BackColor = System.Drawing.Color.Red;
+            this.checkBox1.ForeColor = System.Drawing.SystemColors.Control;
+            this.checkBox1.Location = new System.Drawing.Point(814, 6);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(141, 30);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Auto Inject";
+            this.checkBox1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.checkBox1.UseVisualStyleBackColor = false;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(169, 442);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(97, 39);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Kill Roblox";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.Control;
+            this.button2.Location = new System.Drawing.Point(1010, 5);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(41, 35);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.Control;
+            this.button3.Location = new System.Drawing.Point(962, 5);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(41, 35);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "-";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.Red;
+            this.listBox1.ForeColor = System.Drawing.SystemColors.Control;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(944, 46);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 368);
+            this.listBox1.TabIndex = 10;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseMove);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Red;
+            this.button4.Location = new System.Drawing.Point(962, 380);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "refresh";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // openfile
+            // 
+            this.openfile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.openfile.BackColor = System.Drawing.Color.Red;
+            this.openfile.Location = new System.Drawing.Point(303, 442);
+            this.openfile.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.openfile.Name = "openfile";
+            this.openfile.Size = new System.Drawing.Size(97, 39);
+            this.openfile.TabIndex = 12;
+            this.openfile.Text = "Open";
+            this.openfile.UseVisualStyleBackColor = false;
+            this.openfile.Click += new System.EventHandler(this.openfile_Click);
+            // 
+            // savefile
+            // 
+            this.savefile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.savefile.BackColor = System.Drawing.Color.Red;
+            this.savefile.Location = new System.Drawing.Point(435, 442);
+            this.savefile.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.savefile.Name = "savefile";
+            this.savefile.Size = new System.Drawing.Size(97, 39);
+            this.savefile.TabIndex = 13;
+            this.savefile.Text = "Save";
+            this.savefile.UseVisualStyleBackColor = false;
+            this.savefile.Click += new System.EventHandler(this.savefile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Red;
+            this.ClientSize = new System.Drawing.Size(1065, 493);
+            this.Controls.Add(this.savefile);
+            this.Controls.Add(this.openfile);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.status);
+            this.Controls.Add(this.robloxopen);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Editor);
+            this.Controls.Add(this.Inject);
+            this.Controls.Add(this.execute);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.Control;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Editor)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button execute;
+        private System.Windows.Forms.Button Inject;
+        private FastColoredTextBoxNS.FastColoredTextBox Editor;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label robloxopen;
+        private System.Windows.Forms.Label status;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button openfile;
+        private System.Windows.Forms.Button savefile;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace spudey_executor
+{
+    public partial class Form1 : RoundedForm
+    {
+        Timer time = new Timer();
+        public Point mouseLocation;
+
+        public Form1()
+        {
+            InitializeComponent();
+            time.Tick += timertick;
+            time.Start();
+            ForlornApi.Api.InitializeForlorn();
+
+
+
+        }
+
+        private void timertick(object sender, EventArgs e)
+        {
+            if (ForlornApi.Api.IsRobloxOpen())
+            {
+                robloxopen.Text = "Roblox Open: ✅";
+                robloxopen.ForeColor = Color.LightGreen;  // Change text color to green
+            }
+            else
+            {
+                robloxopen.Text = "Roblox Open: ❌";
+                robloxopen.ForeColor = Color.Black;  // Change text color to red
+            }
+
+            if (ForlornApi.Api.IsInjected())
+            {
+                status.Text = "Status: Injected!";
+                status.ForeColor = Color.LightGreen;  // Change text color to green
+            }
+            else
+            {
+                status.Text = "Status: Not Injected!";
+                status.ForeColor = Color.Black;  // Change text color to red
+            }
+        }
+
+
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ForlornApi.Api.Inject();
+        }
+
+        private void execute_Click(object sender, EventArgs e)
+        {
+            ForlornApi.Api.ExecuteScript(Editor.Text);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void robloxopen_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            ForlornApi.Api.SetAutoInject(true);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ForlornApi.Api.KillRoblox();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Editor.Text = File.ReadAllText($"./Scripts/{listBox1.SelectedItem}");
+        }
+
+        class functions
+        {
+            public static void PopulateListBox(System.Windows.Forms.ListBox lsb, string Folder, string FileType)
+            {
+                DirectoryInfo dinfo = new DirectoryInfo(Folder);
+                FileInfo[] Files = dinfo.GetFiles(FileType);
+                foreach (FileInfo file in Files)
+                {
+                    lsb.Items.Add(file.Name);
+                }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            functions.PopulateListBox(listBox1, "./Scripts", "*.lua");
+            functions.PopulateListBox(listBox1, "./Scripts", "*.txt");
+
+        }
+
+        private void label1_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseLocation = new Point(-e.X, -e.Y);
+        }
+
+        private void listBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                Point mousePose = Control.MousePosition;
+                mousePose.Offset(mouseLocation.X, mouseLocation.Y);
+                Location = mousePose;
+            }
+        }
+
+         private void openfile_Click(object sender, EventArgs e)
+ {
+     OpenFileDialog openFileDialog1 = new OpenFileDialog
+     {
+         Filter = "Lua Files (*.lua)|*.lua|Text Files (*.txt)|*.txt",
+         Title = "Open Lua or Text File"
+     };
+
+     if (openFileDialog1.ShowDialog() == DialogResult.OK)
+     {
+         Editor.Text = File.ReadAllText(openFileDialog1.FileName);
+     }
+ }
+
+        private void savefile_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog
+            {
+                Filter = "Lua Files (*.lua)|*.lua|Text Files (*.txt)|*.txt",
+                DefaultExt = "lua",
+                Title = "Save Lua or Text File"
+            };
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string textToSave = Editor.Text;
+                using (Stream s = File.Open(saveFileDialog1.FileName, FileMode.Create))
+                using (StreamWriter sw = new StreamWriter(s))
+                {
+                    sw.Write(textToSave);
+                }
+            }
+        }
+
+    }
+}
